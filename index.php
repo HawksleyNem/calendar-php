3e8fe805a72b45c763688849a2fbfa276f5cc3c7
Hawksley <hawksley.mqt@gmail.com>
2023-03-10 10:30:06 +0100

    working calendar

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
 <?php
-    $months = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
-    $days = ['Lun','Mar','Mer','Jeu','Ven','Sam','Dim'];
+    date_default_timezone_set('Europe/Paris');
+    $dt = time();
+    $formatter = new IntlDateFormatter('fr_FR');
+    $formatter -> setPattern('eeee');
+
+    $years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2013];
+    $months = [1 => 'Janvier',2 => 'Février',3 => 'Mars',4 => 'Avril',5 => 'Mai',6 => 'Juin',7 => 'Juillet',8 => 'Août',9 => 'Septembre',10 => 'Octobre',11 => 'Novembre',12 => 'Décembre'];
+    $days = [1 => 'Lun',2 => 'Mar',3 => 'Mer',4 => 'Jeu',5 => 'Ven',6 => 'Sam',7 => 'Dim'];
+    date_default_timezone_set('Europe/Paris');
+    $currentDate = isset($_POST['submit']) ? new DateTime($_POST['submit']) : new DateTime();
+
+    if (isset($_POST['submit'])) {
+        $selectedMonth = $_POST['month'];
+        $selectedYear = $_POST['year'];
+        // echo 'La date que vous avez saisie est : ' . $selectedMonth . ' ' . $selectedYear;
+        foreach ($months as $month => $value) {
+            if ($months[$month] == $selectedMonth) {
+                $selectedMonthNb = $month;
+            }
+        };
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN,$selectedMonthNb,$selectedYear);
+
+        $selectedDate = new DateTime($selectedYear . '-' . $selectedMonthNb . '-01');
+        $selectedDate->modify('first day of this month');
+        $firstDayOfMonth = ucwords($formatter->format($selectedDate));
+        $firstDayOfMonth = $firstDayOfMonth[0] . $firstDayOfMonth[1] . $firstDayOfMonth[2];
+
+        foreach ($days as $day => $value) {
+            if ($days[$day] == $firstDayOfMonth) {
+                $firstDayOfMonth = $day;
+            }
+        };
+
+        $selectedDate->modify('last day of this month');
+        $lastDayOfMonth = ucwords($formatter->format($selectedDate));
+        $lastDayOfMonth = $lastDayOfMonth[0] . $lastDayOfMonth[1] . $lastDayOfMonth[2];
+
+        foreach ($days as $day => $value) {
+            if ($days[$day] == $lastDayOfMonth) {
+                $lastDayOfMonth = $day;
+            }
+        };
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,23 +60,44 @@
         <section id="calendar">
             <div class="cal-header">
                 <div class="top-cal-header">
-                    <img src="" alt="" class="arrow">
-                    <h2>Mois XXXX</h2>
-                    <img src="" alt="" class="arrow">
+                    <a href="">
+                        <img src="./assets/img/arrow-left-white.png" alt="Flèche gauche" class="arrow">
+                    </a>
+                    <h2><?= isset($selectedDate) ? $selectedMonth . ' ' . $selectedYear : $currentDate->format('m') . ' ' . $currentDate->format('Y') ?></h2>
+                    <a href="">
+                        <img src="./assets/img/arrow-right-white.png" alt="Flèche droite" class="arrow">
+                    </a>
                 </div>
-                <div class="bottom-cal-header">
-                    <input type="text">
-                    <input type="text">
-                    <button>Valider</button>
-                </div>
+                <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
+                    <div class="bottom-cal-header">
+                        <select name="month" id="month">
+                            <?php for ($i = 1; $i < count($months) + 1; $i++) { ?>
+                                <option value="<?= $months[$i] ?>"><?= $months[$i] ?></option>
+                            <?php } ?>
+                        </select>
+                        <select name="year" id="year">
+                            <?php for ($i = 0; $i < count($years); $i++) { ?>
+                                <option value="<?= $years[$i] ?>"><?= $years[$i] ?></option>
+                            <?php } ?>
+                        </select>
+                        <button name="submit" type="submit">Valider</button>
+                    </div>
+                </form>
             </div>
             <div class="grid">
-                <?php for ($i = 0; $i < 7; $i++) { ?>
+                <?php for ($i = 1; $i <= 7; $i++) { ?>
                     <div class="grid-item day-name"><?= $days[$i] ?></div>
                 <?php } ?>
-                <?php for ($i = 1; $i < 29; $i++) { ?>
+                <?php for ($i = 1; $i < $firstDayOfMonth; $i++) { ?>
+                    <div class="grid-item day-case"></div>
+                <?php } ?>
+                <?php for ($i = 1; $i <= $daysInMonth; $i++) { ?>
                     <div class="grid-item day-case"><?= $i ?></div>
                 <?php } ?>
+                <?php for ($i = $lastDayOfMonth; $i < 7; $i++) { ?>
+                    <div class="grid-item day-case"></div>
+                <?php } ?>
+                
             </div>
         </section>
 
